70322d9f56dc58295040242192702536f3bc7427
zidong.wang <wangzidongprince@163.com>
2016-12-17 16:54:14 +0800

    ready

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,7 +1,5 @@
 "use strict";
 
-var USER_NAME_KEY = "AJAX_IM_USERNAME";
-
 var main  = {
     selfName:"",
     init:function(){
@@ -10,23 +8,13 @@ var main  = {
         this.initListener();
     },
     initPage:function(){
-        $("#talks").html("");
+
     },
     initTimer:function(){
-        var _this = this;
-        setInterval(function(){
-            _this.receiveMsg();
-        },1000);
+
     },
     initUser:function(){
-        var userName = localStorage.getItem(USER_NAME_KEY);
-        if(!userName){
-            $("#registerFrame").show();
-        }else{
-            $("#talkFrame").show();
-            this.selfName = userName;
-            this.initTimer();
-        }
+        $("#registerFrame").show();
     },
     _registe:function(userName){
         var user = {
@@ -34,31 +22,10 @@ var main  = {
         }
         var sex = Math.random()>0.5?"boy":"girl";
         user["sex"]=sex;
-        var url = "regist";
-        var _this=this;
-        this._api(url,user,function(res){
-            if(res.code==0){
-                //注册成功 将名字写入本地
-//                localStorage.setItem(USER_NAME_KEY,userName);
-//                window.location.reload();
-                _this.selfName=userName;
-                $("#registerFrame").hide();
-                $("#talkFrame").show();
-                _this.initTimer();
-            }else{
-                alert(res.desc);
-            }
-        });
+
     },
     renderTalk:function(msgs){
-        var talkHtml = "";
-        var len = msgs.length;
-        for(var i=0;i<len;i++){
-            var msg = msgs[i];
-            talkHtml+=this.renderMsg(msg);
-        }
-        $("#talks").html(talkHtml);
-        $('#talks').scrollTop( $('#talks')[0].scrollHeight );
+
     },
     renderMsg:function(msg){
         var user = msg.user;
@@ -67,22 +34,10 @@ var main  = {
         var html = "";
         if(userName==this.selfName){
             //这是我自己说的话
-            html = ['<div class="line right">',
-                '                    <div class="talk-name">'+userName+'</div>',
-                '                    <div class="paopao">',
-                '                        <font>'+msgContent+'</font>',
-                '                        <div class="arrow"></div>',
-                '                    </div>',
-                '                </div>'].join("");
+
         }else{
             //这是别人说的话
-            html = ['<div class="line left">',
-                '                    <div class="talk-name">'+userName+'</div>',
-                '                    <div class="paopao">',
-                '                        <font>'+msgContent+'</font>',
-                '                        <div class="arrow"></div>',
-                '                    </div>',
-                '                </div>'].join("");
+
         }
         return html;
     },
@@ -92,19 +47,12 @@ var main  = {
             userName:this.selfName
         }
         var url = "send";
-        this._api(url,msgObj,function(res){
-            if(res.code==0){
-                console.log("sendmsg \n"+JSON.stringify(msgObj)+"\nsuccess") ;
-            }
-        });
+
     },
     receiveMsg:function(){
         var url = "receive";
         var _this = this;
-        this._api(url,{},function(res){
-            console.log(res);
-            _this.renderTalk(res);
-        });
+
     },
     _api:function(url,data,callback){
         $.ajax({
@@ -125,17 +73,10 @@ var main  = {
         var _this = this;
         $("#loginBtn").on("click",function(){
             //点击了登录
-            var userName = $("#userName").val().trim();
-            if(userName!=""){
-                _this._registe(userName);
-            }
+
         });
         $("#sendBtn").on("click",function(){
-            var msg = $("#msgInput").val().trim();
-            if(msg!=""){
-                _this.sendMsg(msg);
-                $("#msgInput").val("");
-            }
+            //点击了发送按钮
         });
     }
 }
